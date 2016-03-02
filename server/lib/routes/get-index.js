34c22a697b509762abdb1561b15caecfa0e0b1a0
Phil Booth <pmbooth@gmail.com>
2016-03-02 16:19:03 +0000

    feat(server): emit the new flow.begin activity event

--- server/lib/routes/get-index.js
+++ server/lib/routes/get-index.js
@@ -2,6 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var activityEvent = require('../activity-event');
+var crypto = require('crypto');
+
+var activityEventQueryParams = [
+  'context',
+  'entrypoint',
+  'migration',
+  'service',
+  'utm_campaign',
+  'utm_content',
+  'utm_medium',
+  'utm_source',
+  'utm_term'
+];
 
 module.exports = function (config) {
   var STATIC_RESOURCE_URL = config.get('static_resource_url');
@@ -11,9 +25,19 @@ module.exports = function (config) {
   route.path = '/';
 
   route.process = function (req, res) {
+    var time = Date.now();
+    var flowId = crypto.randomBytes(16).toString('hex');
     res.render('index', {
+      flowBeginTime: time,
+      flowId: flowId,
       staticResourceUrl: STATIC_RESOURCE_URL
     });
+
+    activityEvent('flow.begin', {
+      flow_id: flowId, //eslint-disable-line camelcase
+      flow_time: 0, //eslint-disable-line camelcase
+      time: time
+    }, req, activityEventQueryParams);
   };
 
   return route;
